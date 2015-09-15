60c50000c8043d6a768087536730a29688fbf1af
why <why3046380@163.com>
2015-09-15 15:10:54 +0800

    Initial commit

---
+++ src/main/java/com/domain/UserInfo.java
@@ -0,0 +1,41 @@
+package com.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import  javax.persistence.Id;
+/**
+ * Created by 20150831 on 2015/9/15.
+ */
+@Entity
+public class UserInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  int Id;
+    private String UserName;
+    private  String Password;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+}
