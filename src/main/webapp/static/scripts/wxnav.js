187a61fe60fb693628721b29fccc48a544eda901
why <why3046380@163.com>
2015-09-16 18:22:13 +0800

    测试

---
+++ src/main/webapp/static/scripts/wxnav.js
@@ -0,0 +1,34 @@
+﻿var _menus = {
+    "menus": [
+        {
+            "menuid": "9",
+            "menuname": "微信管理",
+            "menus": [
+                {
+                    "menuid": "91",
+                    "menuname": "自定义按钮（服务号）",
+                    "url": "/Weixin/MenuManage.aspx",
+                    "icon": "icon-nav"
+                },
+                {
+                    "menuid": "92",
+                    "menuname": "群发图文管理（服务号）",
+                    "url": "/Weixin/NewsManage.aspx",
+                    "icon": "icon-nav"
+                },
+                {
+                    "menuid": "93",
+                    "menuname": "租车生活（服务号）",
+                    "url": "/Weixin/NewsLife.aspx",
+                    "icon": "icon-nav"
+                }, {
+                    "menuid": "94",
+                    "menuname": "分享大奖",
+                    "url": "/Weixin/CarLifeList.aspx",
+                    "icon": "icon-nav"
+                }
+            ]
+        }
+
+    ]
+};
