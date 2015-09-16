acec0e87942bfedee9a39e481e37d4a559cfab31
wanghy <why3046380@163.com>
2015-09-16 23:38:30 +0800

    添加了项目

---
+++ src/main/java/com/common/SecurityAspect.java
@@ -0,0 +1,16 @@
+package com.common;
+
+/**
+ * Created by wanghy on 2015/9/16.
+ */
+public class SecurityAspect {
+    public  void BeforeExecute(){
+        System.out.println("before this method execute!");
+    }
+    public  void  AfterExecute(){
+        System.out.println("after this method execute!");
+    }
+    public  void  AfterThrowException(){
+         System.out.println("AfterThrowException this method execute!");
+    }
+}
